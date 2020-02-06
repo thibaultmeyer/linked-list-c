a25c15583e22c9488c1279941672a115346587a7
Thibault Meyer <meyer.thibault@gmail.com>
2020-02-06 20:31:08 +0100

    Enable TravisCI
    Signed-off-by: Thibault Meyer <meyer.thibault@gmail.com>

--- test/main.c
+++ test/main.c
@@ -8,15 +8,23 @@ s_linkedlist_unittest gl_unit_tests[] = {
 };
 
 int main(int argc, char **argv) {
-    unsigned int idx = 0;
-    int          ret = 0;
+    unsigned int idx           = 0;
+    int          ret           = 0;
+    int          success_count = 0;
+    int          error_count   = 0;
 
     while (gl_unit_tests[idx].name != NULL) {
         ret = gl_unit_tests[idx].fun_unit_test();
         printf("[%s] %s\n", ret == 1 ? "PASS" : "FAIL", gl_unit_tests[idx].name);
 
+        if (ret == 1) {
+            success_count += 1;
+        } else {
+            error_count += 1;
+        }
+
         idx += 1;
     }
 
-    return (0);
+    return (error_count);
 }
