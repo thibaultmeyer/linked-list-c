f3f2ba772d83c6bce7b29e75b3db5148e89b229c
Thibault Meyer <meyer.thibault@gmail.com>
2020-02-18 18:35:42 +0100

    Check memory allocation in linkedlist_find
    Signed-off-by: Thibault Meyer <meyer.thibault@gmail.com>

--- src/linkedlist_find.c
+++ src/linkedlist_find.c
@@ -2,8 +2,12 @@
 #include "linkedlist.h"
 
 s_linkedlist *linkedlist_find(s_linkedlist *linked_list, f_matcher matcher, void *matcher_arg) {
-    s_linkedlist      *linked_list_result = linkedlist_create();
     s_linkedlist_node *node               = linked_list->head;
+    s_linkedlist      *linked_list_result = linkedlist_create();
+
+    if (linked_list_result == NULL) {
+        return NULL;
+    }
 
     while (node != NULL) {
         if (matcher(node->data, matcher_arg) == E_MATCHER_RESULT_MATCH) {
--- test/test_linkedlist_size.c
+++ test/test_linkedlist_size.c
@@ -3,19 +3,29 @@
 
 int test_linkedlist_size(void) {
     s_linkedlist *linked_list = NULL;
-    unsigned int size         = 0;
+    unsigned int size_one     = 0;
+    unsigned int size_two     = 0;
+    unsigned int error        = 0;
 
     // Test
     linked_list = linkedlist_create();
     linkedlist_add_front(linked_list, NULL);
     linkedlist_add_front(linked_list, NULL);
     linkedlist_add_front(linked_list, NULL);
+    size_one = linkedlist_size(linked_list);
+    linkedlist_remove(linked_list, 0);
+    size_two = linkedlist_size(linked_list);
 
     // Assert
-    size = linkedlist_size(linked_list);
+    if (size_one != 3) {
+        error += 1;
+    }
+    if (size_two != 2) {
+        error += 1;
+    }
 
     // Free allocated memory
     linkedlist_destroy(linked_list, NULL);
 
-    return (size == 3);
+    return (error == 0);
 }
